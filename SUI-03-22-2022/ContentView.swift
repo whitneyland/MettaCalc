5ff089d6c8867d205c950453f217f921385dd73d
Lee Whitney <lee@whitneyland.com>
2022-03-29 14:04:32 -0500

    Initial Commit

---
+++ SUI-03-22-2022/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SUI-03-22-2022
+//
+//  Created by Lee Whitney on 3/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SUI-03-22-2022/SUI_03_22_2022App.swift
@@ -0,0 +1,17 @@
+//
+//  SUI_03_22_2022App.swift
+//  SUI-03-22-2022
+//
+//  Created by Lee Whitney on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct SUI_03_22_2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
